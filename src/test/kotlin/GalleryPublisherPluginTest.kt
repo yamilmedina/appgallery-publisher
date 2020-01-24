7a6345bff1828df04d4f404e9ded65583aaca669
yamilmedina <yamil.smc@gmail.com>
2020-01-24 09:58:44 -0300

    fix test for application of plugin

--- src/test/kotlin/GalleryPublisherPluginTest.kt
+++ src/test/kotlin/GalleryPublisherPluginTest.kt
@@ -1,5 +1,7 @@
 import com.android.build.gradle.AppExtension
+import com.yamilmedina.gallerypublish.AppGalleryPublisherExtension
 import com.yamilmedina.gallerypublish.AppGalleryPublisherPlugin
+import junit.framework.Assert.assertNotNull
 import org.gradle.api.Project
 import org.gradle.api.internal.plugins.PluginApplicationException
 import org.gradle.api.internal.project.DefaultProject
@@ -25,14 +27,16 @@ class GalleryPublisherPluginTest {
     }
 
     @Test
-    fun pluginDetectsParametersNotSpecified() {
+    fun pluginIsAddedSuccessfullyToProject() {
         project.plugins.apply("com.android.application")
-        project.plugins.apply(AppGalleryPublisherPlugin::class.java)
         prepareAndroidLibraryProject(project)
+        project.plugins.apply(AppGalleryPublisherPlugin::class.java)
+        prepareAppGalleryPublishPlugin(project)
+
         (project as DefaultProject).evaluate()
 
-        //project.tasks.forEach { println(it) }
-        //assertParametersSent
+        val task = project.getTasksByName("appGalleryPublishRelease", false)
+        assertNotNull(task)
     }
 
     private fun prepareAndroidLibraryProject(project: Project) {
@@ -42,5 +46,13 @@ class GalleryPublisherPluginTest {
         val manifestFile = File(project.projectDir, "src/main/AndroidManifest.xml")
         manifestFile.parentFile.mkdirs()
         manifestFile.writeText("""<manifest package="com.foo.bar"/>""")
+    }
+
+    private fun prepareAppGalleryPublishPlugin(project: Project) {
+        val extension = project.extensions.getByType(AppGalleryPublisherExtension::class.java)
+        extension.appId = "123456"
+        extension.clientId = "0987654321"
+        extension.clientSecret = "MF29J23D2K23FAD023D2DK23DL222221"
+        extension.artifactPath = "/tmp/app.apk"
     }
 }
